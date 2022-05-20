e88b98f8f3e649df5541d640040e399500a5654c
Aaron Boyd <a.boyd91@yahoo.com>
2022-05-20 06:08:52 -0700

    add routes

--- app.rb
+++ app.rb
@@ -8,3 +8,85 @@ require('pg')
 require('./db_access.rb')
 
 # DB = PG.connect({:dbname => 'volunteer_tracker'})
+
+get('/results') do
+  erb(:search_results)
+end
+
+post('/results') do
+  title = params[:search]
+  @projects =  Project.search(title) 
+  erb(:search_results)
+end
+
+get('/') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+get ('/projects') do
+  @projects = Project.all
+  erb(:projects)
+end
+
+post('/projects') do
+  title = params[:project_title]
+  project = Project.new(:title => title, :id =>nil)
+  project.save()
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects/new') do
+  erb(:new_project)
+end
+
+get('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
+end
+
+get('/projects/:id/edit') do
+  @project = Project.find(params[:id].to_i())
+  erb(:edit_project)
+end
+
+patch('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.update({:title => params[:title]})
+  @projects = Project.all
+  erb(:projects)
+end
+
+delete('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.delete()
+  @projects = Project.all
+  erb(:projects)
+end
+
+get('/projects/:id/volunteers/:volunteer_id') do
+  @volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  erb(:volunteer)
+end
+
+post('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  volunteer = Volunteer.new(:name => params[:volunteer_name],:project_id => @project.id,:id => nil)
+  volunteer.save()
+  erb(:project)
+end
+
+patch('/projects/:id/volunteers/:volunteer_id') do
+  @project = Project.find(params[:id].to_i())
+  volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  volunteer.update(params[:name], @project.id)
+  erb(:project)
+end
+
+delete('/projects/:id/volunteers/:volunteer_id') do
+  volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  volunteer.delete
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
+end
