0a03de32ff56abc5051557e63446dc6d295db36d
Aaron Boyd <a.boyd91@yahoo.com>
2022-05-20 05:38:14 -0700

    add methods

--- lib/project.rb
+++ lib/project.rb
@@ -7,31 +7,70 @@ class Project
     @id = attributes.fetch(:id)
   end
 
-  def ==()
-
+  def ==(project_to_compare)
+    self.title() == project_to_compare.title()
   end
 
   def self.all
-
+    returned_projects = DB.exec("SELECT * FROM projects;")
+    projects = []
+    returned_projects.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    projects
   end
 
   def save
-
+    result = DB.exec("INSERT INTO projects (title) VALUES ('#{@title}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
   end
 
-  def self.find
-
+  def self.find(id)
+    project = DB.exec("SELECT * FROM projects WHERE id = #{id};").first
+    if project
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      Project.new({:title => title, :id => id})
+    else
+      nil
+    end
   end
 
   def volunteers
-
+    find = Volunteer.find_by_project(self.id)
+    if find.any?
+      find
+    end
   end
 
-  def update
-
+  def update(attributes)
+    if (attributes.has_key?(:title)) && (attributes.fetch(:title) != nil)
+      @title = attributes.fetch(:title)
+      DB.exec("UPDATE projects SET title = '#{@title}' WHERE id = #{@id};")
+    end
   end
 
   def delete
+    DB.exec("DELETE FROM projects WHERE id = #{@id};")
+    DB.exec("DELETE FROM volunteers WHERE project_id = #{@id};")
+  end
 
+  def self.clear
+    DB.exec("DELETE FROM projects *;")
+  end
+
+  def self.search(str)
+    result = DB.exec("SELECT * FROM projects WHERE title ILIKE '%#{str}%';")
+    projects = []
+    result.each() do |project|
+      title = project.fetch("title")
+      id = project.fetch("id").to_i
+      projects.push(Project.new({:title => title, :id => id}))
+    end
+    if projects.any?
+      projects
+    end
   end
 end
